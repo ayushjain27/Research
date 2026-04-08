5c7d8ae219ecfbdee6c7790238e694c78ee9eee5
AayushJain27 <ayush@serviceplug.in>
2026-04-08 19:40:22 +0530

    new chnags

--- src/component/TermsAndCondition.js
+++ src/component/TermsAndCondition.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ArrowRight, ExternalLink } from "lucide-react";
+import { ExternalLink } from "lucide-react";
 import HeaderTab from "./HeaderTab";
 
 const TermsAndConditions = () => {
